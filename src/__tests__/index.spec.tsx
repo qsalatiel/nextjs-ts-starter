37c33f4ee09f14d18705bbc827f254251b29e261
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2022-03-08 15:08:15 -0300

    :tada: First commit!

---
+++ src/__tests__/index.spec.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByTestId("home-title");
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("NextJS Typescript Starter");
+  });
+});
